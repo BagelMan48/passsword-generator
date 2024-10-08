306721eaa74846e518099ab3f1129c8fde5cf6c6
BagelMan48 <182304799+BagelMan48@users.noreply.github.com>
2024-10-07 20:13:17 -0700

    Update password_generator.py
    Added encryption + giving the user the ability to see what passwords are saved.

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,7 @@
 from random import randint
 import json
 import os
+import base64
 
 def passwordGenerator(length, wantChar):
     password = ""
@@ -22,6 +23,51 @@ def passwordGenerator(length, wantChar):
 
 def main():
 
+    userOption = input("Do you want to generate a password or view saved passwords? G for Generate. V for View: ")
+    while (userOption.lower() != 'g') and (userOption.lower() != "generate") and (userOption.lower() != 'v') and (userOption.lower() != "view"):
+        userOption = input("Invalid input. Please enter G for Generate or V for View: ")
+    if (userOption.lower() == 'v') or (userOption.lower() == "view"):
+        if not os.path.exists("passwords.json"):
+            print("No passwords saved.")
+            return
+        else:
+            with open("passwords.json", 'r') as file:
+                passData = json.load(file)
+                for key in passData:
+                    print(key)
+                file.close()
+            passLabel = input("Which password would you like to view? ")
+            with open("passwords.json", 'r') as file:
+                passData = json.load(file)
+                if passLabel in passData:
+                    print("Password for " + passLabel + ": " + passData[passLabel]["Password"])
+                    if passData[passLabel]["isEncrypted"]:
+                        decrypt = input("Would you like to decrypt this password? Y for Yes. N for No: ").lower()
+                        if decrypt == 'y' or decrypt == 'yes':
+                            password = base64.b64decode(passData[passLabel]["Password"]).decode('utf-8')
+                            passData[passLabel]["Password"] = password
+                            passData[passLabel]["isEncrypted"] = False
+                            print("Decrypted password: " + password)
+                            with open("passwords.json", 'w') as file:
+                                json.dump(passData, file)
+                                file.close()
+                    else:
+                        print("Password is not encrypted. Would you like to encrypt it? Y for Yes. N for No: ")
+                        encrypt = input().lower()
+                        if encrypt == 'y' or encrypt == 'yes':
+                            password = base64.b64encode(passData[passLabel]["Password"].encode('utf-8')).decode('utf-8')
+                            passData[passLabel]["Password"] = password
+                            passData[passLabel]["isEncrypted"] = True
+                            print("Password has been encrypted. \nEncrypted password: " + password)
+                            with open("passwords.json", 'w') as file:
+                                json.dump(passData, file)
+                                file.close()
+                else:
+                    print("No password for " + passLabel + " found.")
+                file.close()
+            return
+
+
     wantChar = False
 
     lengthInput = int(input("Enter the length of your password: "))
@@ -38,28 +84,34 @@ Y for Yes. N for No: """)
 
     passSave = input("""Do you want to save your password?
 Y for Yes. N for No: """)
-    
     if (passSave.lower() == 'y') or (passSave.lower() == "yes"):
         passLabel = input("What are you saving this password for? ")
 
+        passEncrypted = input("Would you like to encrypt your password? Y for Yes. N for No: ").lower()
+        if passEncrypted == 'y' or passEncrypted == 'yes':
+            password = base64.b64encode(password.encode('utf-8')).decode('utf-8')
+            print("Password encrypted.")
+            passEncrypted = True
+
+
         passDataCurrent = {
-            passLabel : password
+            passLabel : {"Password" : password, "isEncrypted": passEncrypted}
         }
         
 
-        if not os.path.exists("password.json"):
-            with open("password.json", 'w') as file:
+        if not os.path.exists("passwords.json"):
+            with open("passwords.json", 'w') as file:
                 json.dump(passDataCurrent, file)
                 file.close()
 
         else:
-            with open("password.json", 'r+') as file:
+            with open("passwords.json", 'r+') as file:
                 content = file.read()
                 if not content:
                     file.seek(0)
                     json.dump(passDataCurrent, file)
                 else:
-                    passData = json.loads(content)
+                    passData = json.loads(content) if content else {}
                     if passLabel in passData:
                         updateConfirm = input("A password for " + passLabel + " has already been made. \nDo you want to overwrite it? ")
                         if (updateConfirm.lower() == 'y') or (updateConfirm.lower() == "yes"):
@@ -68,7 +120,7 @@ Y for Yes. N for No: """)
                             file.truncate()
                             json.dump(passData, file)
                     else:
-                        passData[passLabel] = password
+                        passData[passLabel] = {"Password" : password, "isEncrypted": passEncrypted}
                         file.seek(0)
                         file.truncate()
                         json.dump(passData, file)
